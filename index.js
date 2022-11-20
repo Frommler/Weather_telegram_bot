9fa91e855e0fce4c658f75c92fa49ad731a08cde
Alex Vakula <albanec777@meta.ua>
2022-11-20 13:21:46 +0100

    TODO, check time every minute

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const { Telegraf } = require("telegraf"),
   bot = new Telegraf(TOKEN);
 
 let cityPerId = {};
+let userHoursPerId = {};
+let userMinPerId = {};
+
+/* user.name = "Alex";
+user[name] = "Alex";
+user[12412412414] = "Alex";
+cityPerId[98765433456789] = "London";
+cityPerId.98765433456789 = "London"; */
 
 bot.start((ctx) => {
   ctx.replyWithHTML("Choise your city: ", {
@@ -18,16 +26,76 @@ bot.start((ctx) => {
     },
   });
 });
-//регулярний вираз / контекст
+//-------регулярний вираз / контекст
 bot.action(/^setCity-(\w+)/, (ctx) => {
   cityPerId[ctx.chat.id] = ctx.match[1];
   console.log(cityPerId);
+  ctx.replyWithHTML("Set your time: ", {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "Own time", callback_data: "setOwnTime" }],
+        [
+          { text: "9:00", callback_data: "setTime-9" },
+          { text: "10:00", callback_data: "setTime-10" },
+          { text: "11:00", callback_data: "setTime-11" },
+          { text: "12:00", callback_data: "setTime-12" },
+        ],
+      ],
+    },
+  });
+});
+
+bot.action(/^setTime-(\d+)/, (ctx) => {
+  userHoursPerId[ctx.chat.id] = ctx.match[1];
+  userMinPerId[ctx.chat.id] = "00";
+  ctx.reply("You set time - " + userHoursPerId[ctx.chat.id] + ":00");
   setInterval(() => {
-    fetch("http://localhost:9000/weather/" + cityPerId[ctx.chat.id])
+    checkTime(ctx);
+  }, 60000);
+});
+
+bot.action("setOwnTime", (ctx) => {
+  ctx.reply("Input your time in format HH:MM");
+});
+
+//TODO - check hours am pm
+
+bot.hears(/^([0-1]?[0-9]|2[0-3]):([0-5][0-9])$/, (ctx) => {
+  userHoursPerId[ctx.chat.id] = ctx.match[1];
+  userMinPerId[ctx.chat.id] = ctx.match[2];
+  setInterval(() => {
+    console.log("setInterval");
+    checkTime(ctx);
+  }, 60000);
+  ctx.reply("You set time - " + ctx.match[0]);
+});
+
+bot.launch();
+
+function checkTime(ctxObj) {
+  let now = new Date();
+  console.log("CHECK TIME");
+  console.log(userHoursPerId[ctxObj.chat.id] + "=" + now.getHours());
+  console.log(userMinPerId[ctxObj.chat.id] + "=" + now.getMinutes());
+
+  if (
+    Number(userHoursPerId[ctxObj.chat.id]) == now.getHours() &&
+    Number(userMinPerId[ctxObj.chat.id]) == now.getMinutes()
+  ) {
+    console.log("Go to serwer" + cityPerId);
+    fetch("http://localhost:9000/weather/" + cityPerId[ctxObj.chat.id])
       .then((response) => response.json())
       .then((data) => {
-        ctx.reply("Temperature in " + data.city + ": " + data.temp);
+        console.log(data);
+        ctxObj.reply("Temperature in " + data.city + ": " + data.temp);
       });
-  }, 30000);
-});
-bot.launch();
+  }
+}
+
+/* setInterval(() => {
+  fetch("http://localhost:9000/weather/" + cityPerId[ctx.chat.id])
+    .then((response) => response.json())
+    .then((data) => {
+      ctx.reply("Temperature in " + data.city + ": " + data.temp);
+    });
+}, 30000); */
